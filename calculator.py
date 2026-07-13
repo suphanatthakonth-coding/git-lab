d850e184236f1994a1ec617ccd6516c0cde05caf
suphanatthakonth-coding <suphanatthakon.th@gmail.com>
2026-07-13 15:16:09 +0700

    V3: multiply function

--- calculator.py
+++ calculator.py
@@ -2,5 +2,13 @@ def add(a, b):
     return a+b
 print(add(2,3))
 
+#note: V3 has NO subtract
+def add(a, b):
+    return a + b
 
+def multiply(a, b):
+    return a * b
+
+print(add(2, 3))
+print(multiply(4, 5))
 
